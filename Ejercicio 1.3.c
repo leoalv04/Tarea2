9402336b283c93b2ce11056d29a28ed07da6fbe8
leoalv04 <159968841+leoalv04@users.noreply.github.com>
2025-11-19 15:29:40 -0600

    Create Ejercicio 1.3.c

---
+++ Ejercicio 1.3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  float d,p;
+  printf("Cantidad en dlls: ");
+  scanf ("%f", &d);
+
+  p = d*(11.96);
+  printf("Equivalente en pesos: %.2f\n", p);
+ 
+
+return 0;
+}
